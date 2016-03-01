a7b857b86feb6a4422e8ff7cca3bee1c9403f06b
JožeJerše <ragnar1724@gmail.com>
2016-03-01 13:53:43 +0000

    variables

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <body>
     drek je drek
     <?php
-echo "Hello Worlderolololojkljll!";
+echo "Hello Worlderolololojkljll!\n";
 echo "yolo";
 echo "yolo";
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
 </body>
 </html>
