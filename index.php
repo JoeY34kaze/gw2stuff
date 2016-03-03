1c7d19bdb962e65da235f0e783fd6c97fa8e0556
JožeJerše <ragnar1724@gmail.com>
2016-03-03 14:11:36 +0000

    8 min in

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<html>
+<!doctype html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet"/>
+    <title>JoeY34kaze's gw2 stuff</title>
 <body>
+    <div id="container">
+        <header>
+            <h1>
+                This is my header.
+            </h1>
+        </header>
+        <div id="banner">
+            <h2>
+                This is the banner.
+            </h2>
+        </div>
+        <div> id="content">
+            <p>Content goes here.</p>
+        </div>
+    </div>
+    
+    
     drek je drek
     <?php
 echo "Hello Worlderolololojkljll!";
